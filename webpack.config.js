0cdf29003d37b0f50fa2588863e354de605e424d
Marcin Treder <marcin@uxpin.com>
2017-11-30 18:20:43 -0800

    Correcting issues with webpack, eslint and heroku;

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,41 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: [
-    './src/index.js'
-  ],
+  entry: ['./src/index.js'],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/' // build
+    publicPath: '/', // build
   },
   resolve: {
     modules: [__dirname, 'node_modules'],
-    extensions: ['*','.js','.jsx']
+    extensions: ['*', '.js', '.jsx'],
   },
   devServer: {
-		historyApiFallback: true
-	},
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
-        use: ['babel-loader', 'eslint-loader'],
+        use: ['babel-loader'], // 'eslint-loader'
         test: /\.jsx?$/,
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         use: ['file-loader'],
-        test: /\.(jpe?g|png|gif)$/i
+        test: /\.(jpe?g|png|gif)$/i,
       },
       {
         use: ['raw-loader'],
-        test: /\.svg$/
-      }
-    ]
+        test: /\.svg$/,
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'src/index.html'
+      template: 'src/index.html',
     }),
-  ]
+  ],
 };
-
 
 module.exports = config;

5610aad580bfcf575eeec100946b6a0afc63be4a
Marcin Treder <marcin@uxpin.com>
2017-11-30 18:39:32 -0800

    Change in webpack build public path

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/', // build
+    publicPath: '/build', // build
   },
   resolve: {
     modules: [__dirname, 'node_modules'],

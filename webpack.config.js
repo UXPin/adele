42d49d626cf7f681de22296aeea474d0a1b9e098
Marcin Treder <marcin@uxpin.com>
2017-12-29 12:56:11 +0100

    Changes to Webpack Config. Two environments defined in the config file

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,49 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const config = {
-  entry: ['./src/index.js'],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/build', // build
-  },
-  resolve: {
-    modules: [__dirname, 'node_modules'],
-    extensions: ['*', '.js', '.jsx'],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  module: {
-    rules: [
-      {
-        use: ['babel-loader', 'eslint-loader'],
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-      },
-      {
-        use: ['file-loader'],
-        test: /\.(jpe?g|png|gif)$/i,
-      },
-      {
-        use: ['raw-loader'],
-        test: /\.svg$/,
-      },
+module.exports = (env = {}) => {
+  const isProduction = env.production === true;
+  const isLocal = env.local === true;
+  return {
+    entry: ['./src/index.js'],
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'bundle.js',
+      publicPath: (() => {
+        if (isProduction) return '/build';
+        else if (isLocal) return './';
+        return '/';
+      })(),
+    },
+    resolve: {
+      modules: [__dirname, 'node_modules'],
+      extensions: ['*', '.js', '.jsx'],
+    },
+    devServer: {
+      historyApiFallback: true,
+      compress: true,
+    },
+    module: {
+      rules: [
+        {
+          use: ['babel-loader', 'eslint-loader'],
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+        },
+        {
+          use: ['file-loader'],
+          test: /\.(jpe?g|png|gif)$/i,
+        },
+        {
+          use: ['raw-loader'],
+          test: /\.svg$/,
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-  ],
+  };
 };
-
-module.exports = config;

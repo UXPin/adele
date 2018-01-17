ec02368d884344498356f32a439f345b7dc312ca
Marcin Treder <marcin@uxpin.com>
2018-01-16 20:11:30 -0800

    Added robots.txt to allow Twitter bot to access image

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env = {}) => {
   const isProduction = env.production === true;
@@ -45,6 +46,7 @@ module.exports = (env = {}) => {
       new HtmlWebpackPlugin({
         template: 'src/index.html',
       }),
+      new CopyWebpackPlugin([{ from: './robots.txt', to: './' }]),
     ],
   };
 };

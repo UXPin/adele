7fbd083eb7a120ffd6ee6d790007ec96acf52071
Marcin Treder <marcin@uxpin.com>
2018-01-16 19:04:54 -0800

    Added automatic year assignment to copyrights in the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import { StyledFooter, StyledTextSection } from './footer.styles';
 import Logo from '../logo/logo';
 
-const Footer = () => (
-  <StyledFooter>
-    <Logo tab={5} />
-    <p>2010 - 2017 INC</p>
-    <StyledTextSection>
-      <li>Maintained by Marcin Treder.</li>
-      <li>
-        Inspired by{' '}
-        <a href="https://github.com/alexpate/awesome-design-systems">Awesome Design Systems</a> by
-        Alex Pate.
-      </li>
-    </StyledTextSection>
-  </StyledFooter>
-);
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <StyledFooter>
+      <Logo tab={5} />
+      <p>2010 - {currentYear} UXPin INC</p>
+      <StyledTextSection>
+        <li>Maintained by Marcin Treder.</li>
+        <li>
+          Inspired by{' '}
+          <a href="https://github.com/alexpate/awesome-design-systems">Awesome Design Systems</a> by
+          Alex Pate.
+        </li>
+      </StyledTextSection>
+    </StyledFooter>
+  );
+};
 
 export { Footer as default };

64df1ccc57a68c2efa5152c04cc1fe5096febe10
Michał Stocki <michal.stocki@uxpin.com>
2018-01-23 00:34:00 +0100

    data-rebuilder: Removed file stat based operations

--- tools/data-rebuilder.js
+++ tools/data-rebuilder.js
@@ -15,19 +15,8 @@ const data = fs.readFileSync(dataJSON, 'utf8', (err, obj) => {
 
 const dataObj = JSON.parse(data);
 
-dataObj.forEach((system, i) => {
+dataObj.forEach((system) => {
   // create filename from names of companies
   const filename = system.company.data.replace(/\s+/g, '-').toLowerCase();
-  fs.writeFileSync(`${directoryPath}/${filename}.JSON`, JSON.stringify(system, null, 2), 'utf-8');
-
-  // modify time stamps to keep the order from the original data file
-
-  const filePath = `${directoryPath}/${filename}.JSON`;
-
-  // get atime and mtime
-  const atime = fs.statSync(filePath).atime.getTime();
-  const mtime = fs.statSync(filePath).mtime.getTime();
-
-  // modify atime and mtime
-  fs.utimesSync(filePath, atime, mtime - i);
+  fs.writeFileSync(`${directoryPath}/${filename}.json`, JSON.stringify(system, null, 2), 'utf-8');
 });

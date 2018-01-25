dca91672e6882b5e63ad840ff6465eb8a8e48651
Marcin Treder <marcin@uxpin.com>
2018-01-25 10:59:50 -0800

    changed name for ant design to the new structure and regenerated data.json

--- tools/utils/getTimestampFromFilePath.js
+++ tools/utils/getTimestampFromFilePath.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = function getTimestampFromFilePath(filePath) {
   const filename = path.basename(filePath, '.json');
-  return (/^(\d{12})-.*/g).exec(filename)[1];
+  return /^(\d{12})-.*/g.exec(filename)[1];
 };

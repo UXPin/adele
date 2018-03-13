71211fab6265e206f2b1c40a65b41834bf2d1a94
Marcin Treder <marcin@uxpin.com>
2018-03-12 20:39:01 -0500

    Correction of the twitter card image

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,10 @@ module.exports = (env = {}) => {
         template: 'src/index.html',
         favicon: './src/assets/favicon.ico',
       }),
-      new CopyWebpackPlugin([{ from: './robots.txt', to: './' }]),
+      new CopyWebpackPlugin([
+        { from: './robots.txt', to: './' },
+        { from: './src/assets/twitter_image.jpg', to: './' },
+      ]),
     ],
   };
 };

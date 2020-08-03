56bf3ddd31498d6a41ce54f44b726ed438da4c0e
plaszczkowski <piotr.laszczkowski@uxpin.com>
2020-08-03 14:41:33 +0200

    #97 - updates tests specs

--- src/components/logo/tests/logo.test.jsx
+++ src/components/logo/tests/logo.test.jsx
@@ -1,10 +1,11 @@
 import { mount } from 'enzyme';
 import React from 'react';
 import 'jest-styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import Logo from '../logo';
 
 it('renders Logo correctly', () => {
-  const wrapper = mount(<Logo />);
+  const wrapper = mount(<BrowserRouter><Logo /></BrowserRouter>);
   expect(wrapper).toMatchSnapshot();
 });

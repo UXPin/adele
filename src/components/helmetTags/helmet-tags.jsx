dea6dc00dbca28f2c14440ac04bdea77906ebcdf
naohiro-t <31594089+naohiro-t@users.noreply.github.com>
2021-06-18 18:19:05 +0900

    Revert "Fix broken twitter url (#121)"
    This reverts commit 55d48767b5d8bf57893114a1a8ae5c39b294c2cd.

--- src/components/helmetTags/helmet-tags.jsx
+++ src/components/helmetTags/helmet-tags.jsx
@@ -9,7 +9,7 @@ const HelmetTags = ({ title, description, urlNoSpecialCharacters }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@marcintreder" />
       <meta name="twitter:site" content="@uxpin" />
-      <meta name="twitter:image" content="https://adele.uxpin.com/twitter_image.jpg" />
+      <meta name="twitter:image" content="https://adele.uxpin.com/build/twitter_image.jpg" />
       <meta name="twitter:image:width" content="600" />
       <meta name="twitter:image:height" content="215" />
       <meta name="twitter:url" content={urlNoSpecialCharacters} />

79228e8d7efb36e76b05ef3f361985a7686609a3
Nick Roma <54879724+nikoandpiko@users.noreply.github.com>
2021-09-10 21:01:43 +0900

    Adds viewport to all pages (#124)

--- src/components/helmetTags/helmet-tags.jsx
+++ src/components/helmetTags/helmet-tags.jsx
@@ -6,6 +6,7 @@ const HelmetTags = ({ title, description, urlNoSpecialCharacters }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content="@marcintreder" />
       <meta name="twitter:site" content="@uxpin" />
